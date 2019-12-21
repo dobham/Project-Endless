89a9c81a494384997535f28e1bc3879bdc0f1960
Mahbod <mahbodsabbaghi@gmail.com>
2019-12-21 16:28:50 -0500

    -Refractoring -Spawning capablities of Boids -Average heading function created

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+//public class Boid : MonoBehaviour
+//{
+//    //Initialize the basics for each BOID
+//    public static float alignmentForce = 1;
+//    public static float avoidForce = 1;
+//    public static float collisionForce = 10;
+//    private static Vector3 position;
+//    private static float viewRadius = 5;
+//    private static float avoidRadius = 2;
+//    private static float collisionRadius = 5;
+//    private static float minSpeed = 3;
+//    private static float maxSpeed = 5;
+//    private static float goldenRatio = (1 + Mathf.Sqrt (5)) / 2;
+//    private static float angleIncrement = Mathf.PI * 2 * goldenRatio;
+//    private Vector3[] directions;
+//    private const int numViewDirections = 300;
+//    private Vector3 boidDirection;
+//    public GameObject boidModel;
+//    
+//    void setModel()
+//    {
+//        Instantiate(boidModel, new Vector3(0, 0, 0), Quaternion.identity);
+//    }
+//
+//    void setDirection()
+//    {
+//        boidDirection = transform.forward;
+//    }
+//
+//    public void Start()
+//    {
+//        setModel();
+//        setDirection();
+//    }
+//    
+//    //Function for finding average heading  : FUNCTION 1
+//    static Vector3 averageHeading(Boid[] boidArray)
+//    {
+//        Vector3[] headings = new Vector3[boidArray.Length];
+//        Vector3 headingSum = new Vector3();
+//        for (int i = 0 ; i < boidArray.Length; i++)
+//        {
+//            headings[i] = boidArray[i].boidDirection;
+//        }
+//        for (int i = 0 ; i < boidArray.Length; i++)
+//        {
+//            headingSum += headings[i];
+//        }
+//        Vector3 averageHeading = headingSum / boidArray.Length;
+//        return averageHeading;
+//    }
+//    //Cycle through all BOIDS, if one is in the viewRadius, add their heading value to an array
+//    //Take said array and average all of the values and make it its own heading value
+//    static void boidDirections () {
+//        Vector3[] directions = new Vector3[numViewDirections];
+//        for (int i = 0; i < numViewDirections; i++) {
+//            float t = (float) i / numViewDirections;
+//            float inclination = Mathf.Acos (1 - 2 * t);
+//            float azimuth = angleIncrement * i;
+//
+//            float x = Mathf.Sin (inclination) * Mathf.Cos (azimuth);
+//            float y = Mathf.Sin (inclination) * Mathf.Sin (azimuth);
+//            float z = Mathf.Cos (inclination);
+//            directions[i] = new Vector3 (x, y, z);
+//        }
+//    }
+//    //Function for BOID avoidance  : FUNCTION 2
+//    static void boidAvoid(Boid[] boidArray)
+//    {
+//        
+//    }
+//    //Cycle through all BOIDS, if one is in the avoidRadius, change heading and speed accordingly
+//
+//    //Function for obstacle avoidance  : FUNCTION 3
+//    static void obstacleAvoid()
+//    {
+//        
+//    }
+//    //Cast out rays to locate obstacle, if obstacle is near, change heading and speed
+//    
+//    
+//}
--- Assets/Scripts/CubeGenerator.cs
+++ Assets/Scripts/CubeGenerator.cs
@@ -3,26 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CubeGenerator : MonoBehaviour {
+public class CubeGenerator : MonoBehaviour
+{
     public Material material;
     public static int subdivisions = 9;
     Vector3[] verts = new Vector3[subdivisions * subdivisions * 6];
-    int[] triangleVerts = new int[(subdivisions-1)*(subdivisions-1) * 36];
+    int[] triangleVerts = new int[(subdivisions - 1) * (subdivisions - 1) * 36];
     Vector3[] normals = new Vector3[subdivisions * subdivisions * 6];
-    void Start() {
-        
+
+    void Start()
+    {
         MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
         MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
         Mesh cubeSphere = new Mesh();
 
         int index = 0;
-        for (var i = 0; i < 6; i++) {
-            switch (i) {
+        for (var i = 0; i < 6; i++)
+        {
+            switch (i)
+            {
                 //top square
                 case 0:
-                    for (int x = -Mathf.FloorToInt(subdivisions/2f); x < Mathf.CeilToInt(subdivisions/2f); x++) {
-                        for (var y = -Mathf.FloorToInt(subdivisions/2f); y < Mathf.CeilToInt(subdivisions/2f); y++, index++) {
-                            normals[index] = verts[index] = new Vector3(x, y, Mathf.FloorToInt(subdivisions/2f)).normalized;
+                    for (int x = -Mathf.FloorToInt(subdivisions / 2f); x < Mathf.CeilToInt(subdivisions / 2f); x++)
+                    {
+                        for (var y = -Mathf.FloorToInt(subdivisions / 2f);
+                            y < Mathf.CeilToInt(subdivisions / 2f);
+                            y++, index++)
+                        {
+                            normals[index] = verts[index] =
+                                new Vector3(x, y, Mathf.FloorToInt(subdivisions / 2f)).normalized;
 //                            float xVal = v.x * v.x;
 //                            float yVal = v.y * v.y;
 //                            float zVal = v.z * v.z;
@@ -34,12 +43,19 @@ public class CubeGenerator : MonoBehaviour {
 //                            verts[index] = normals[index];
 //                            print(verts[index]);
                         }
-                    } break;
+                    }
+
+                    break;
                 //right square (when looking from above)
                 case 1:
-                    for (var y = -Mathf.FloorToInt(subdivisions/2f); y < Mathf.CeilToInt(subdivisions/2f); y++) {
-                        for (var z = -Mathf.FloorToInt(subdivisions/2f); z < Mathf.CeilToInt(subdivisions/2f); z++, index++) {
-                            normals[index] = verts[index] = new Vector3(Mathf.FloorToInt(subdivisions/2f), y, z).normalized;
+                    for (var y = -Mathf.FloorToInt(subdivisions / 2f); y < Mathf.CeilToInt(subdivisions / 2f); y++)
+                    {
+                        for (var z = -Mathf.FloorToInt(subdivisions / 2f);
+                            z < Mathf.CeilToInt(subdivisions / 2f);
+                            z++, index++)
+                        {
+                            normals[index] = verts[index] =
+                                new Vector3(Mathf.FloorToInt(subdivisions / 2f), y, z).normalized;
 //                            float xVal = verts[index].x * verts[index].x;
 //                            float yVal = verts[index].y * verts[index].y;
 //                            float zVal = verts[index].z * verts[index].z;
@@ -49,12 +65,19 @@ public class CubeGenerator : MonoBehaviour {
 //                            s.z = verts[index].z * Mathf.Sqrt(1f - xVal / 2f - yVal / 2f + xVal * yVal / 3f);
 //                            normals[index] = s;
                         }
-                    } break;
+                    }
+
+                    break;
                 //left sqaure
                 case 2:
-                    for (var y = Mathf.FloorToInt(subdivisions/2f); y > -Mathf.CeilToInt(subdivisions/2f); y--) {
-                        for (var z = -Mathf.FloorToInt(subdivisions/2f); z < Mathf.CeilToInt(subdivisions/2f); z++, index++) {
-                            normals[index] = verts[index] = new Vector3(-Mathf.FloorToInt(subdivisions/2f), y, z).normalized;
+                    for (var y = Mathf.FloorToInt(subdivisions / 2f); y > -Mathf.CeilToInt(subdivisions / 2f); y--)
+                    {
+                        for (var z = -Mathf.FloorToInt(subdivisions / 2f);
+                            z < Mathf.CeilToInt(subdivisions / 2f);
+                            z++, index++)
+                        {
+                            normals[index] = verts[index] =
+                                new Vector3(-Mathf.FloorToInt(subdivisions / 2f), y, z).normalized;
 //                            float xVal = verts[index].x * verts[index].x;
 //                            float yVal = verts[index].y * verts[index].y;
 //                            float zVal = verts[index].z * verts[index].z;
@@ -64,12 +87,19 @@ public class CubeGenerator : MonoBehaviour {
 //                            s.z = verts[index].z * Mathf.Sqrt(1f - xVal / 2f - yVal / 2f + xVal * yVal / 3f);
 //                            normals[index] = s;
                         }
-                    } break;
+                    }
+
+                    break;
                 //front sqaure
                 case 3:
-                    for (var x = -Mathf.FloorToInt(subdivisions/2f); x < Mathf.CeilToInt(subdivisions/2f); x++) {
-                        for (var z = -Mathf.FloorToInt(subdivisions/2f); z < Mathf.CeilToInt(subdivisions/2f); z++, index++) {
-                            normals[index] = verts[index] = new Vector3(x, -Mathf.FloorToInt(subdivisions/2f), z).normalized;
+                    for (var x = -Mathf.FloorToInt(subdivisions / 2f); x < Mathf.CeilToInt(subdivisions / 2f); x++)
+                    {
+                        for (var z = -Mathf.FloorToInt(subdivisions / 2f);
+                            z < Mathf.CeilToInt(subdivisions / 2f);
+                            z++, index++)
+                        {
+                            normals[index] = verts[index] =
+                                new Vector3(x, -Mathf.FloorToInt(subdivisions / 2f), z).normalized;
 //                            float xVal = verts[index].x * verts[index].x;
 //                            float yVal = verts[index].y * verts[index].y;
 //                            float zVal = verts[index].z * verts[index].z;
@@ -79,12 +109,19 @@ public class CubeGenerator : MonoBehaviour {
 //                            s.z = verts[index].z * Mathf.Sqrt(1f - xVal / 2f - yVal / 2f + xVal * yVal / 3f);
 //                            normals[index] = s;
                         }
-                    } break;
+                    }
+
+                    break;
                 //back square
                 case 4:
-                    for (var x = Mathf.FloorToInt(subdivisions/2f); x > -Mathf.CeilToInt(subdivisions/2f); x--) {
-                        for (var z = -Mathf.FloorToInt(subdivisions/2f); z < Mathf.CeilToInt(subdivisions/2f); z++, index++) {
-                            normals[index] = verts[index] = new Vector3(x, Mathf.FloorToInt(subdivisions/2f), z).normalized;
+                    for (var x = Mathf.FloorToInt(subdivisions / 2f); x > -Mathf.CeilToInt(subdivisions / 2f); x--)
+                    {
+                        for (var z = -Mathf.FloorToInt(subdivisions / 2f);
+                            z < Mathf.CeilToInt(subdivisions / 2f);
+                            z++, index++)
+                        {
+                            normals[index] = verts[index] =
+                                new Vector3(x, Mathf.FloorToInt(subdivisions / 2f), z).normalized;
 //                            float xVal = verts[index].x * verts[index].x;
 //                            float yVal = verts[index].y * verts[index].y;
 //                            float zVal = verts[index].z * verts[index].z;
@@ -94,12 +131,19 @@ public class CubeGenerator : MonoBehaviour {
 //                            s.z = verts[index].z * Mathf.Sqrt(1f - xVal / 2f - yVal / 2f + xVal * yVal / 3f);
 //                            normals[index] = s;
                         }
-                    } break;
+                    }
+
+                    break;
                 //bottom square
                 default:
-                    for (var x = Mathf.FloorToInt(subdivisions/2f); x > -Mathf.CeilToInt(subdivisions/2f); x--) {
-                        for (var y = -Mathf.FloorToInt(subdivisions/2f); y < Mathf.CeilToInt(subdivisions/2f); y++, index++) {
-                            normals[index] = verts[index] = new Vector3(x, y,-Mathf.FloorToInt(subdivisions/2f)).normalized;
+                    for (var x = Mathf.FloorToInt(subdivisions / 2f); x > -Mathf.CeilToInt(subdivisions / 2f); x--)
+                    {
+                        for (var y = -Mathf.FloorToInt(subdivisions / 2f);
+                            y < Mathf.CeilToInt(subdivisions / 2f);
+                            y++, index++)
+                        {
+                            normals[index] = verts[index] =
+                                new Vector3(x, y, -Mathf.FloorToInt(subdivisions / 2f)).normalized;
 //                            float xVal = verts[index].x * verts[index].x;
 //                            float yVal = verts[index].y * verts[index].y;
 //                            float zVal = verts[index].z * verts[index].z;
@@ -109,7 +153,9 @@ public class CubeGenerator : MonoBehaviour {
 //                            s.z = verts[index].z * Mathf.Sqrt(1f - xVal / 2f - yVal / 2f + xVal * yVal / 3f);
 //                            normals[index] = s;
                         }
-                    } break;
+                    }
+
+                    break;
             }
         }
 
@@ -120,43 +166,52 @@ public class CubeGenerator : MonoBehaviour {
         cubeSphere.vertices = verts;
         cubeSphere.normals = normals;
         index = 0;
-        for (int face = 0; face < 6; face++) {
-            for (int xIndex = 0; xIndex < subdivisions-1; xIndex++) {
-                for (int yIndex = 0; yIndex < subdivisions-1; yIndex++) {
+        for (int face = 0; face < 6; face++)
+        {
+            for (int xIndex = 0; xIndex < subdivisions - 1; xIndex++)
+            {
+                for (int yIndex = 0; yIndex < subdivisions - 1; yIndex++)
+                {
                     triangleVerts[index] = (face * subdivisions * subdivisions) + (xIndex * subdivisions) + yIndex;
                     index++;
-                    
-                    triangleVerts[index] = (face * subdivisions * subdivisions) + (xIndex * subdivisions) + (yIndex + subdivisions + 1);
+
+                    triangleVerts[index] = (face * subdivisions * subdivisions) + (xIndex * subdivisions) +
+                                           (yIndex + subdivisions + 1);
                     index++;
-                    
-                    triangleVerts[index] = (face * subdivisions * subdivisions) + (xIndex * subdivisions) + (yIndex + 1);
+
+                    triangleVerts[index] =
+                        (face * subdivisions * subdivisions) + (xIndex * subdivisions) + (yIndex + 1);
                     index++;
-                    
+
                     triangleVerts[index] = (face * subdivisions * subdivisions) + (xIndex * subdivisions) + yIndex;
                     index++;
-                    
-                    triangleVerts[index] = (face * subdivisions * subdivisions) + (xIndex * subdivisions) + (yIndex + subdivisions);
+
+                    triangleVerts[index] = (face * subdivisions * subdivisions) + (xIndex * subdivisions) +
+                                           (yIndex + subdivisions);
                     index++;
-                    
-                    triangleVerts[index] = (face * subdivisions * subdivisions) + (xIndex * subdivisions) + (yIndex + subdivisions + 1);
+
+                    triangleVerts[index] = (face * subdivisions * subdivisions) + (xIndex * subdivisions) +
+                                           (yIndex + subdivisions + 1);
                     index++;
                 }
-            } 
+            }
         }
+
         cubeSphere.triangles = triangleVerts;
-        
+
         cubeSphere.RecalculateNormals();
         meshFilter.mesh = cubeSphere;
         meshRenderer.material = material;
 
 
-        for (int i = 0; i < (subdivisions-1)*(subdivisions-1) * 36; i++)
+        for (int i = 0; i < (subdivisions - 1) * (subdivisions - 1) * 36; i++)
         {
 //            print(i);
             print(triangleVerts[i]);
         }
 
-        for (int i = 0; i < subdivisions * subdivisions * 6; i++) {
+        for (int i = 0; i < subdivisions * subdivisions * 6; i++)
+        {
             GameObject dube = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             dube.transform.localScale = Vector3.one * 0.2f;
 
