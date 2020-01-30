094c82607d8358f89aed859b07038fa30d1d4c0c
Mahbod <mahbodsabbaghi@gmail.com>
2020-01-29 20:23:24 -0500

    -Added rotation -Implemented Transformation

--- Assets/Scripts/BoidMaster.cs
+++ Assets/Scripts/BoidMaster.cs
@@ -8,7 +8,7 @@ public class BoidMaster : MonoBehaviour
     // Start is called before the first frame update
     private const int NumBoids = 10; 
     private readonly GameObject[] _boids = new GameObject[NumBoids];
-    private readonly BoidObject[] _boidScripts = new BoidObject[NumBoids];
+    private readonly BoidObject[] _boidObjects = new BoidObject[NumBoids];
     public GameObject boid;
     
     void Start()
@@ -18,9 +18,8 @@ public class BoidMaster : MonoBehaviour
         for (var i = 0; i < NumBoids; i++)
         {
             _boids[i] = GameObject.Instantiate(boid, new Vector3(controllerPosition.x + Rand.Next(-10,10), controllerPosition.y + Rand.Next(-10,10), controllerPosition.z + Rand.Next(-10,10)), Quaternion.identity);
-            _boidScripts[i] = _boids[i].GetComponent<BoidObject>();
+            _boidObjects[i] = _boids[i].GetComponent<BoidObject>();
         }
-        
     }
 
     // Update is called once per frame
--- Assets/Scripts/BoidObject.cs
+++ Assets/Scripts/BoidObject.cs
@@ -1,15 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BoidObject : MonoBehaviour
 {
     private static readonly Random Rand = new Random();
     
-    public Vector3 direction;
+    public Vector3 directionCurrent;
+    public Vector3 directionTarget;
     public Vector3 position;
-    public float speed = 10;
+    public float speed = 0;
+    private Transform objectTransform;
     
     private float _viewRadius = 30;
     private float _obstacleRadius = 45;
+
+    private void Start()
+    {
+        objectTransform = transform;
+        directionCurrent = Vector3.up;
+        directionTarget = Vector3.left;
+        
+    }
+
+    private void Update()
+    {
+        if (directionTarget.x > directionCurrent.x)
+        {
+            directionCurrent.x += 0.01f;
+        }
+        else directionCurrent.x -= 0.01f;
+        
+        if (directionTarget.y > directionCurrent.y)
+        {
+            directionCurrent.y += 0.01f;
+        }
+        else directionCurrent.y -= 0.01f;
+        
+        if (directionTarget.z > directionCurrent.z)
+        {
+            directionCurrent.z += 0.01f;
+        }
+        else directionCurrent.z -= 0.01f;
+
+        objectTransform.position += directionCurrent;
+        objectTransform.rotation = Quaternion.LookRotation(directionCurrent);
+    }
 }
