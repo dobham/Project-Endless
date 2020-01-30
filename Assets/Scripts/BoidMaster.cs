cd1dbfe6267548aa0981a2fc64f95e6e5b5c22a4
Mahbod <mahbodsabbaghi@gmail.com>
2020-01-29 19:32:11 -0500

    -Bug Fixes

--- Assets/Scripts/BoidMaster.cs
+++ Assets/Scripts/BoidMaster.cs
@@ -7,15 +7,17 @@ public class BoidMaster : MonoBehaviour
     // Start is called before the first frame update
     public const int NumBoids = 10; 
     private readonly GameObject[] _boids = new GameObject[NumBoids];
-    public GameObject boid = new GameObject();
+    private readonly BoidObject[] _boidScripts = new BoidObject[NumBoids];
+    public GameObject boid;
+    
     void Start()
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < NumBoids; i++)
         {
             _boids[i] = GameObject.Instantiate(boid);
+            _boidScripts[i] = _boids[i].GetComponent<BoidObject>();
         }
-
-        _boids[1].GetComponent<BoidObject>().direction = new Vector3();
+        
     }
 
     // Update is called once per frame
