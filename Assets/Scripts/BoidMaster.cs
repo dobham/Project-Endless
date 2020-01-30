f5639bab0d46a5a160c95a8d7b3827f26cc3e744
Mahbod <mahbodsabbaghi@gmail.com>
2020-01-29 21:20:00 -0500

    -Implemented Average Heading

--- Assets/Scripts/BoidMaster.cs
+++ Assets/Scripts/BoidMaster.cs
@@ -6,9 +6,10 @@ using Random = System.Random;
 public class BoidMaster : MonoBehaviour
 {
     // Start is called before the first frame update
-    private const int NumBoids = 10; 
-    private readonly GameObject[] _boids = new GameObject[NumBoids];
-    private readonly BoidObject[] _boidObjects = new BoidObject[NumBoids];
+    public int NumBoids = 10;
+    private const int boidsNum = 10;
+    public readonly GameObject[] Boids = new GameObject[boidsNum];
+    public readonly BoidObject[] BoidObjects = new BoidObject[boidsNum];
     public GameObject boid;
     
     void Start()
@@ -17,8 +18,8 @@ public class BoidMaster : MonoBehaviour
         var controllerPosition = transform.position; 
         for (var i = 0; i < NumBoids; i++)
         {
-            _boids[i] = GameObject.Instantiate(boid, new Vector3(controllerPosition.x + Rand.Next(-10,10), controllerPosition.y + Rand.Next(-10,10), controllerPosition.z + Rand.Next(-10,10)), Quaternion.identity);
-            _boidObjects[i] = _boids[i].GetComponent<BoidObject>();
+            Boids[i] = GameObject.Instantiate(boid, new Vector3(controllerPosition.x + Rand.Next(-10,10), controllerPosition.y + Rand.Next(-10,10), controllerPosition.z + Rand.Next(-10,10)), Quaternion.identity);
+            BoidObjects[i] = Boids[i].GetComponent<BoidObject>();
         }
     }
 
--- Assets/Scripts/BoidObject.cs
+++ Assets/Scripts/BoidObject.cs
@@ -2,27 +2,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Random = UnityEngine.Random;
+using Random = System.Random;
 
 public class BoidObject : MonoBehaviour
 {
-    private static readonly Random Rand = new Random();
-    
     public Vector3 directionCurrent;
     public Vector3 directionTarget;
     public Vector3 position;
     public float rotationSpeed = 0.01f;
+    public float movementSpeed = 0.01f; 
     private Transform objectTransform;
+    public static GameObject Controller;
+    private GameObject[] _boids;
+    private readonly BoidMaster _materScript = Controller.GetComponent<BoidMaster>();
+
+    private Vector3[] _observedPositions;
     
     private float _viewRadius = 30;
     private float _obstacleRadius = 45;
 
     private void Start()
     {
+        Random rand = new Random();
         objectTransform = transform;
-        directionCurrent = Vector3.up;
-        directionTarget = Vector3.left;
-        
+        directionCurrent = new Vector3(rand.Next(-10,10), rand.Next(-10,10), rand.Next(-10,10));
+        directionTarget = new Vector3(rand.Next(-10,10), rand.Next(-10,10), rand.Next(-10,10));
+        _boids = _materScript.Boids;
     }
 
     private void Update()
@@ -44,8 +49,31 @@ public class BoidObject : MonoBehaviour
             directionCurrent.z += rotationSpeed;
         }
         else directionCurrent.z -= rotationSpeed;
-
-        objectTransform.position += directionCurrent;
+        
+        objectTransform.position += directionCurrent * movementSpeed;
         objectTransform.rotation = Quaternion.LookRotation(directionCurrent);
+
+        for (var i = 0; i < _materScript.NumBoids; i++)
+        {
+            if (Vector3.Distance(_materScript.BoidObjects[i].position, position) < _viewRadius)
+            {
+                _observedPositions[i] = _materScript.BoidObjects[i].position;
+            }
+        }
+        AverageHeading(_observedPositions, _materScript.NumBoids);
     }
+
+    public void AverageHeading(Vector3[] boidPositions, int numBoids)
+    {
+        Vector3 average = new Vector3();
+        for (var i = 0; i < numBoids; i++)
+        {
+            average += boidPositions[i];
+        }
+
+        average /= numBoids;
+        
+        directionTarget = average;
+    }
+    
 }
