d09c11c5abd1ad8cb91b3dbdc57d4f5668764c41
Mahbod <mahbodsabbaghi@gmail.com>
2020-01-29 19:27:22 -0500

    -Refactoring in-progress

---
+++ Assets/Scripts/BoidMaster.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidMaster : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public const int NumBoids = 10; 
+    private readonly GameObject[] _boids = new GameObject[NumBoids];
+    public GameObject boid = new GameObject();
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            _boids[i] = GameObject.Instantiate(boid);
+        }
+
+        _boids[1].GetComponent<BoidObject>().direction = new Vector3();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/BoidObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidObject : MonoBehaviour
+{
+    private static readonly Random Rand = new Random();
+    
+    public Vector3 direction = new Vector3();
+    public Vector3 position = new Vector3(1,1,1);
+    public float speed = 10;
+    
+    private float _viewRadius = 30;
+    private float _obstacleRadius = 45;
+}
